af65b06ca8d3d5ab159b6fc606ffb21315f3b3db
Sasz Kolomon <skolomon@hotmail.com>
2023-09-29 20:09:31 +0300

    Arxiv theme

---
+++ plugins/themes/arxiv/ArxivThemePlugin.php
@@ -0,0 +1,264 @@
+<?php
+
+/**
+ * @file plugins/themes/arxiv/ArxivThemePlugin.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
+ * Copyright (c) 2023 Sasz Kolomon
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class ArxivThemePlugin
+ *
+ * @brief Arxiv theme
+ */
+
+namespace APP\plugins\themes\arxiv;
+
+use APP\core\Application;
+use APP\file\PublicFileManager;
+use PKP\config\Config;
+use PKP\plugins\ThemePlugin;
+use PKP\session\SessionManager;
+
+class ArxivThemePlugin extends ThemePlugin
+{
+    /**
+     * @copydoc ThemePlugin::isActive()
+     */
+    public function isActive()
+    {
+        if (SessionManager::isDisabled()) {
+            return true;
+        }
+        return parent::isActive();
+    }
+
+    /**
+     * Initialize the theme's styles, scripts and hooks. This is run on the
+     * currently active theme and it's parent themes.
+     *
+     */
+    public function init()
+    {
+        // Register theme options
+        $this->addOption('typography', 'FieldOptions', [
+            'type' => 'radio',
+            'label' => __('plugins.themes.arxiv.option.typography.label'),
+            'description' => __('plugins.themes.arxiv.option.typography.description'),
+            'options' => [
+                [
+                    'value' => 'notoSans',
+                    'label' => __('plugins.themes.arxiv.option.typography.notoSans'),
+                ],
+                [
+                    'value' => 'notoSerif',
+                    'label' => __('plugins.themes.arxiv.option.typography.notoSerif'),
+                ],
+                [
+                    'value' => 'notoSerif_notoSans',
+                    'label' => __('plugins.themes.arxiv.option.typography.notoSerif_notoSans'),
+                ],
+                [
+                    'value' => 'notoSans_notoSerif',
+                    'label' => __('plugins.themes.arxiv.option.typography.notoSans_notoSerif'),
+                ],
+                [
+                    'value' => 'lato',
+                    'label' => __('plugins.themes.arxiv.option.typography.lato'),
+                ],
+                [
+                    'value' => 'lora',
+                    'label' => __('plugins.themes.arxiv.option.typography.lora'),
+                ],
+                [
+                    'value' => 'lora_openSans',
+                    'label' => __('plugins.themes.arxiv.option.typography.lora_openSans'),
+                ],
+            ],
+            'default' => 'notoSans',
+        ]);
+
+        $this->addOption('baseColour', 'FieldColor', [
+            'label' => __('plugins.themes.arxiv.option.colour.label'),
+            'description' => __('plugins.themes.arxiv.option.colour.description'),
+            'default' => '#4385E5',
+        ]);
+
+        $this->addOption('showDescriptionInServerIndex', 'FieldOptions', [
+            'label' => __('manager.setup.contextSummary'),
+            'options' => [
+                [
+                    'value' => true,
+                    'label' => __('plugins.themes.arxiv.option.showDescriptionInServerIndex.option'),
+                ],
+            ],
+            'default' => false,
+        ]);
+        $this->addOption('useHomepageImageAsHeader', 'FieldOptions', [
+            'label' => __('plugins.themes.arxiv.option.useHomepageImageAsHeader.label'),
+            'description' => __('plugins.themes.arxiv.option.useHomepageImageAsHeader.description'),
+            'options' => [
+                [
+                    'value' => true,
+                    'label' => __('plugins.themes.arxiv.option.useHomepageImageAsHeader.option')
+                ],
+            ],
+            'default' => false,
+        ]);
+        $this->addOption('displayStats', 'FieldOptions', [
+            'type' => 'radio',
+            'label' => __('plugins.themes.arxiv.option.displayStats.label'),
+            'options' => [
+                [
+                    'value' => 'none',
+                    'label' => __('plugins.themes.arxiv.option.displayStats.none'),
+                ],
+                [
+                    'value' => 'bar',
+                    'label' => __('plugins.themes.arxiv.option.displayStats.bar'),
+                ],
+                [
+                    'value' => 'line',
+                    'label' => __('plugins.themes.arxiv.option.displayStats.line'),
+                ],
+            ],
+            'default' => 'none',
+        ]);
+
+        // Load primary stylesheet
+        $this->addStyle('stylesheet', 'styles/index.less');
+
+        // Store additional LESS variables to process based on options
+        $additionalLessVariables = [];
+
+        if ($this->getOption('typography') === 'notoSerif') {
+            $this->addStyle('font', 'styles/fonts/notoSerif.less');
+            $additionalLessVariables[] = '@font: "Noto Serif", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", sans-serif;';
+        } elseif (strpos($this->getOption('typography'), 'notoSerif') !== false) {
+            $this->addStyle('font', 'styles/fonts/notoSans_notoSerif.less');
+            if ($this->getOption('typography') == 'notoSerif_notoSans') {
+                $additionalLessVariables[] = '@font-heading: "Noto Serif", serif;';
+            } elseif ($this->getOption('typography') == 'notoSans_notoSerif') {
+                $additionalLessVariables[] = '@font: "Noto Serif", serif;@font-heading: "Noto Sans", serif;';
+            }
+        } elseif ($this->getOption('typography') == 'lato') {
+            $this->addStyle('font', 'styles/fonts/lato.less');
+            $additionalLessVariables[] = '@font: Lato, sans-serif;';
+        } elseif ($this->getOption('typography') == 'lora') {
+            $this->addStyle('font', 'styles/fonts/lora.less');
+            $additionalLessVariables[] = '@font: Lora, serif;';
+        } elseif ($this->getOption('typography') == 'lora_openSans') {
+            $this->addStyle('font', 'styles/fonts/lora_openSans.less');
+            $additionalLessVariables[] = '@font: "Open Sans", sans-serif;@font-heading: Lora, serif;';
+        } else {
+            $this->addStyle('font', 'styles/fonts/notoSans.less');
+        }
+
+        // Update colour based on theme option
+        if ($this->getOption('baseColour') !== '#4385E5') {
+            $additionalLessVariables[] = '@bg-base:' . $this->getOption('baseColour') . ';';
+            if (!$this->isColourDark($this->getOption('baseColour'))) {
+                $additionalLessVariables[] = '@text-bg-base:rgba(0,0,0,0.84);';
+                $additionalLessVariables[] = '@bg-base-border-color:rgba(0,0,0,0.2);';
+            }
+        }
+
+        // Pass additional LESS variables based on options
+        if (!empty($additionalLessVariables)) {
+            $this->modifyStyle('stylesheet', ['addLessVariables' => join($additionalLessVariables)]);
+        }
+
+        $request = Application::get()->getRequest();
+
+        // Load icon font FontAwesome - http://fontawesome.io/
+        $this->addStyle(
+            'fontAwesome',
+            $request->getBaseUrl() . '/lib/pkp/styles/fontawesome/fontawesome.css',
+            ['baseUrl' => '']
+        );
+
+        // Get homepage image and use as header background if useAsHeader is true
+        $context = Application::get()->getRequest()->getContext();
+        if ($context && $this->getOption('useHomepageImageAsHeader')) {
+            $publicFileManager = new PublicFileManager();
+            $publicFilesDir = $request->getBaseUrl() . '/' . $publicFileManager->getContextFilesPath($context->getId());
+
+            $homepageImage = $context->getLocalizedData('homepageImage');
+
+            $homepageImageUrl = $publicFilesDir . '/' . $homepageImage['uploadName'];
+
+            $this->addStyle(
+                'homepageImage',
+                '.pkp_structure_head { background: center / cover no-repeat url("' . $homepageImageUrl . '");}',
+                ['inline' => true]
+            );
+        }
+
+        // Load jQuery from a CDN or, if CDNs are disabled, from a local copy.
+        $min = Config::getVar('general', 'enable_minified') ? '.min' : '';
+        $jquery = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jquery/jquery' . $min . '.js';
+        $jqueryUI = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jqueryui/jquery-ui' . $min . '.js';
+        // Use an empty `baseUrl` argument to prevent the theme from looking for
+        // the files within the theme directory
+        $this->addScript('jQuery', $jquery, ['baseUrl' => '']);
+        $this->addScript('jQueryUI', $jqueryUI, ['baseUrl' => '']);
+
+        // Load Bootsrap's dropdown
+        $this->addScript('popper', 'js/lib/popper/popper.js');
+        $this->addScript('bsUtil', 'js/lib/bootstrap/util.js');
+        $this->addScript('bsDropdown', 'js/lib/bootstrap/dropdown.js');
+
+        // Load custom JavaScript for this theme
+        $this->addScript('arxiv', 'js/main.js');
+
+        // Add navigation menu areas for this theme
+        $this->addMenuArea(['primary', 'user']);
+    }
+
+    /**
+     * Get the name of the settings file to be installed on new server
+     * creation.
+     *
+     * @return string
+     */
+    public function getContextSpecificPluginSettingsFile()
+    {
+        return $this->getPluginPath() . '/settings.xml';
+    }
+
+    /**
+     * Get the name of the settings file to be installed site-wide when
+     * OPS is installed.
+     *
+     * @return string
+     */
+    public function getInstallSitePluginSettingsFile()
+    {
+        return $this->getPluginPath() . '/settings.xml';
+    }
+
+    /**
+     * Get the display name of this plugin
+     *
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.themes.arxiv.name');
+    }
+
+    /**
+     * Get the description of this plugin
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return __('plugins.themes.arxiv.description');
+    }
+}
+
+if (!PKP_STRICT_MODE) {
+    class_alias('\APP\plugins\themes\arxiv\ArxivThemePlugin', '\ArxivThemePlugin');
+}
---
+++ plugins/themes/arxiv/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file plugins/themes/arxiv/index.php
+ *
+ * Copyright (c) 2014-2022 Simon Fraser University
+ * Copyright (c) 2003-2022 John Willinsky
+ * Copyright (c) 2023 Sasz Kolomon
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @brief Wrapper for arxiv theme plugin.
+ *
+ */
+
+return new \APP\plugins\themes\default\ArxivThemePlugin();
